a602c365462781f1a2d7c22dc0f160a64a0810c1
Elias Reid <47339667+eliasreid@users.noreply.github.com>
2021-04-18 17:04:18 -0300

    hook into load enemy mon

--- src/CrystalAiForm.cs
+++ src/CrystalAiForm.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
+using System.Linq;
 
 using BizHawk.Client.Common;
 using BizHawk.WinForms.Controls;
@@ -13,8 +14,6 @@ namespace BizHawk.Tool.CrystalCtrl
     [ExternalToolApplicability.SingleSystem(CoreSystem.GameBoy)]
     public sealed class CrystalAiForm : Form, IExternalToolForm
     {
-        private Button button1;
-        private Label label1;
 
         const uint BattleMode = 0xD116;
 
@@ -52,7 +51,7 @@ namespace BizHawk.Tool.CrystalCtrl
         {
             [typeof(ICommApi)] = _maybeCommAPI ?? throw new NullReferenceException(),
             [typeof(IEmuClientApi)] = _maybeClientAPI ?? throw new NullReferenceException(),
-            [typeof(IEmulationApi)] = _maybeEmuAPI ?? throw new NullReferenceException(),
+            [typeof(IEmulationApi)] = _maybeEmuAPI ?? throw     new NullReferenceException(),
             [typeof(IGameInfoApi)] = _maybeGameInfoAPI ?? throw new NullReferenceException(),
             [typeof(IGuiApi)] = _maybeGuiAPI ?? throw new NullReferenceException(),
             [typeof(IMemoryApi)] = _maybeMemAPI ?? throw new NullReferenceException(),
@@ -71,15 +70,11 @@ namespace BizHawk.Tool.CrystalCtrl
 
         public CrystalAiForm()
         {
-
-            
-            Console.WriteLine("Hello World write line");
             Text = "Hello, world!";
             SuspendLayout();
             Controls.Add(new LabelEx { Text = "loaded" });
             InitializeComponent();
             MemoryCallbackFlags flags;
-            
 
             //How to get core / rom loaded callback
 
@@ -91,43 +86,38 @@ namespace BizHawk.Tool.CrystalCtrl
         /// </summary>
         public void Restart() {
 
+            Console.WriteLine("Restart called");
+
             //In case of battlemode - good enough to read the value at the end of the frame
             _maybeMemoryEventsAPI.AddWriteCallback((_, written_val, flags) => {
-                //var read_val = _maybeMemAPI.ReadByte(addr);
                 battleModeChanged = true;
                 Console.WriteLine("BattleMode written");
-                //Console.WriteLine($"battle mode written value: {written_val}");
-                //Console.WriteLine($"battle mode read value: {read_val}");
-
-                //Pretty sure flags just gives us the callback type. But already know, because of functino name
-
-
-                //AccessExecute
-
-                //_maybeClientAPI.Pause();
             }, BattleMode, "System Bus");
 
 
-            //Rom Already loaded, so don't have to worry about this
-            //_apis.EmuClient.RomLoaded += romLoaded;
-            //_apis.EmuClient.RomLoaded += (_, _) => { Console.WriteLine(" does lambda callback work? rom loaded"); };
-        }
+            //This is to rewrite wCurPartyMon in LoadEnemyMonToSwitchTo before it is used
+            //TODO: Should be a breakpoint?
+            UInt16 CurMonWriteAddress = 0x5589;
+            List<byte> ExpectedData = new List<byte> { 0x21, 0x7C, 0xDD };
+            _maybeMemoryEventsAPI.AddExecCallback((_, cbAddr, _) =>
+            {
+                //Reading bytes at the program counter location and comparing with data I know should be there
+                //This is a hacky way to make sure we're in the correct ROM bank
+                var bytes = _maybeMemAPI.ReadByteRange(CurMonWriteAddress, 3, "System Bus");
+                if (bytes.SequenceEqual(ExpectedData))
+                {
+                    //TODO: pause emulation - Set up UI for move selection
+                    Console.WriteLine("enemy selecting poke");
+                }
 
-        private void romLoaded(object sender, EventArgs e)
-        {
-            
-        }
+            }, CurMonWriteAddress, "System Bus");
 
+        }
 
 		public bool AskSaveChanges() => true;
 
-		
-
 		public void UpdateValues(ToolFormUpdateType type)
 		{
-            //What does FastPreFrame mean?
-			//if (type != ToolFormUpdateType.PreFrame && type != ToolFormUpdateType.FastPreFrame) return;
-
             switch (type)
             {
                 case ToolFormUpdateType.PostFrame:
@@ -156,50 +146,13 @@ namespace BizHawk.Tool.CrystalCtrl
                 default:
                     break;
             }
-			//TODO do stuff
 		}
 
         private void InitializeComponent()
         {
-            Console.WriteLine($"init components");
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(29, 50);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(48, 102);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
-            // 
-            // CrystalAiForm
-            // 
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
-            this.Name = "CrystalAiForm";
             this.ResumeLayout(false);
             this.PerformLayout();
-            Console.WriteLine($"done init components");
         }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
     }
 }
